b420f9325dc02c176975c34b5bc27f3d8855283b
jhoisz <jvrda@ufpi.edu.br>
2022-08-18 09:29:39 -0300

    #3 Criando models iniciais

---
+++ chegaja_frontend/lib/screens/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home ChegaJa'),
+      ),
+      body: const Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
